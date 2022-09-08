3661bf83de540a48c1cb03e16c07d21184ff1108
Jandueza <jonathanandueza1201@gmail.com>
2022-09-07 22:56:09 -0300

    First Commit

---
+++ js/main.js
@@ -0,0 +1,15 @@
+const slidesContainer = document.getElementById("slides-container");
+const slide = document.querySelector(".slider__slide");
+const prevButton = document.getElementById("slide-arrow-prev");
+const nextButton = document.getElementById("slide-arrow-next");
+
+nextButton.addEventListener("click", () => {
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft += slideWidth;
+});
+
+prevButton.addEventListener("click", () => {
+    const slideWidth = slide.clientWidth;
+    slidesContainer.scrollLeft -= slideWidth;
+});
+
