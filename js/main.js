661489158cd71afb1875e41b1367820c2578b135
Jandueza <jonathanandueza1201@gmail.com>
2022-09-11 19:46:05 -0300

    Third Commit

--- js/main.js
+++ js/main.js
@@ -1,7 +1,11 @@
+// 1. Variables
+
 const slidesContainer = document.getElementById("slides-container");
 const slide = document.querySelector(".slider__slide");
 const prevButton = document.getElementById("slide-arrow-prev");
 const nextButton = document.getElementById("slide-arrow-next");
+
+// 2. Event Listeners
 
 nextButton.addEventListener("click", () => {
     const slideWidth = slide.clientWidth;
@@ -13,3 +17,12 @@ prevButton.addEventListener("click", () => {
     slidesContainer.scrollLeft -= slideWidth;
 });
 
+// 3. Timers
+
+const nextSlideTimeout = setInterval(() => {
+    nextButton.click();
+}, 5000);
+
+// const contactUsTimeout = setTimeout(() => {
+//     document.getElementById("contact-us-button").click();
+// }, 10000);
