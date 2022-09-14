c3719908ec8a83f38b57518cf5a430a84e6abbf6
Jandueza <jonathanandueza1201@gmail.com>
2022-09-14 11:56:07 -0300

    Third Commit

--- js/main.js
+++ js/main.js
@@ -4,6 +4,7 @@ const slidesContainer = document.getElementById("slides-container");
 const slide = document.querySelector(".slider__slide");
 const prevButton = document.getElementById("slide-arrow-prev");
 const nextButton = document.getElementById("slide-arrow-next");
+const scrollToTopButton = document.querySelector(".back-to-top-button");
 
 // 2. Event Listeners
 
@@ -17,6 +18,8 @@ prevButton.addEventListener("click", () => {
     slidesContainer.scrollLeft -= slideWidth;
 });
 
+scrollToTopButton.addEventListener("click", topFunction);
+
 // 3. Timers
 
 const nextSlideTimeout = setInterval(() => {
@@ -26,3 +29,20 @@ const nextSlideTimeout = setInterval(() => {
 // const contactUsTimeout = setTimeout(() => {
 //     document.getElementById("contact-us-button").click();
 // }, 10000);
+
+// 4. Functions
+
+function scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        scrollToTopButton.style.display = "block";
+    } else {
+        scrollToTopButton.style.display = "none";
+    }
+}
+
+window.onscroll = function () { scrollFunction() };
+
+function topFunction() {
+    document.body.scrollTop = 0; // For Safari
+    document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
